aef7593b4086fe18cd8169220365b8cdbb1bc973
ArturStange <artur.stange@sempreceub.com>
2025-08-04 23:53:57 -0300

    Bronze raw and ingot added

--- src/main/java/net/stange/mtmod/datagen/ModBlockTagProvider.java
+++ src/main/java/net/stange/mtmod/datagen/ModBlockTagProvider.java
@@ -2,6 +2,6 @@ package net.stange.mtmod.datagen;
 
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
 
-public class ModBlockTagProvider extends FabricTagProvider.BlockTagProvider {
+//public class ModBlockTagProvider extends FabricTagProvider.BlockTagProvider {
 
-}
+//}
